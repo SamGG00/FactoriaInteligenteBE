61f93b4d31035c5340e0d9c0cc90d52582f4d372
SamGG00 <samir.1701825098@ucaldas.edu.co>
2025-03-01 11:27:39 -0500

    Corrección de nombre para Railway

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 console.log("📌 Conectando a la base de datos...");
 console.log(`DB_HOST: ${process.env.DB_HOST}`);
 console.log(`DB_USER: ${process.env.DB_USER}`);
-console.log(`DB_PORT: ${process.env.DB_PORT}`);
+console.log(`DB_PORT: ${process.env.PORT}`);
 console.log(`DB_NAME: ${process.env.DB_NAME}`);
 
 const pool = mysql.createPool({
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306, // Asegurar que el puerto es un número
+  port: process.env.PORT ? parseInt(process.env.PORT) : 3306, // Asegurar que el puerto es un número
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
