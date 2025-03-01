a88e478582d1212a9e0a5c3f9f92e099694e3700
SamGG00 <samir.1701825098@ucaldas.edu.co>
2025-03-01 10:19:13 -0500

    Correccion de puerto para Railway

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const app = require('./app'); // Importar la aplicación configurada
+const app = require("./app"); // Importar la aplicación configurada
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
