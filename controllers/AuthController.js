97a035de5ac0249fb3fac81dd3e6f9085b5fec79
SamGG00 <samir.1701825098@ucaldas.edu.co>
2025-03-03 22:41:45 -0500

    Cambiando el controller auth true a false

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,38 +1,38 @@
 
-const express= require('express')
-const {authUserService}= require('../services/authService')
-const {generateToken}=require('../services/tokenService')
-const jwt=require("jsonwebtoken")
+const express = require('express')
+const { authUserService } = require('../services/authService')
+const { generateToken } = require('../services/tokenService')
+const jwt = require("jsonwebtoken")
 
 
-const authController= async (req,res) => {
-    const {username,password}=req.body
-    if (!username || !password){
-        return res.status(400).json({ message: 'Todos los campos son obligatorios' })
-    }
-    
-    const user = await authUserService(username,password)
-    if (!user.status) {
-        return res.status(401).json({ message: "Credenciales inválidas" });
-      }
-    
-    
-    const payload = {
-        id: user.id,
-        username: user.user,
-       
-     };
-    const token = generateToken(payload, "5h"); // Token válido por 5 h
-    
-    res.cookie('authToken', token, {
-      httpOnly: true, // Previene el acceso desde JavaScript
-  secure: false, // Cambiar a true solo en producción (cuando uses HTTPS)
- /*  sameSite: 'None',  */// Esto es importante si estás trabajando con un frontend en otro dominio
-  maxAge: 5 * 60 * 60 * 1000, // 5 horas
-      });
+const authController = async (req, res) => {
+  const { username, password } = req.body
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Todos los campos son obligatorios' })
+  }
+
+  const user = await authUserService(username, password)
+  if (!user.status) {
+    return res.status(401).json({ message: "Credenciales inválidas" });
+  }
 
 
-    return res.status(200).json({ status:true, message: "Inicio de sesión exitoso",user:user.user,id:user.id })
+  const payload = {
+    id: user.id,
+    username: user.user,
+
+  };
+  const token = generateToken(payload, "5h"); // Token válido por 5 h
+
+  res.cookie('authToken', token, {
+    httpOnly: true, // Previene el acceso desde JavaScript
+    secure: true, // Cambiar a true solo en producción (cuando uses HTTPS)
+    /*  sameSite: 'None',  */// Esto es importante si estás trabajando con un frontend en otro dominio
+    maxAge: 5 * 60 * 60 * 1000, // 5 horas
+  });
+
+
+  return res.status(200).json({ status: true, message: "Inicio de sesión exitoso", user: user.user, id: user.id })
 
 }
 
@@ -52,4 +52,4 @@ const validateToken = (req, res) => {
 };
 
 
-module.exports = {authController,validateToken}
+module.exports = { authController, validateToken }
